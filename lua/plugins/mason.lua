3d7773b8d3ece6c41908201a5eb5cc11123e1200
ejminguez <erroljamesminguez59@gmail.com>
2025-05-11 00:53:07 +0800

    fix: mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,27 +1,16 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ast_grep",
-          "tailwindcss",
-          "dockerls",
-          "html",
-          "jsonls",
-          "harper_ls",
-          "sqls",
-          "vuels",
-          "eslint",
-        },
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				PATH = "prepend",
+			})
+		end,
+	},
 }
